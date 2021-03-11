578dd813b87549f2bdfbcd891c873a8d3f36f0b1
motoki6318 <mksk6318@icloud.com>
2021-03-11 16:50:09 +0900

    tags, tag_camp_relationsテーブルを作成

---
+++ app/models/camp_tag_relation.rb
@@ -0,0 +1,2 @@
+class CampTagRelation < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_071006) do
+ActiveRecord::Schema.define(version: 2021_03_11_073902) do
+
+  create_table "camp_tag_relations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "camp_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["camp_id"], name: "index_camp_tag_relations_on_camp_id"
+    t.index ["tag_id"], name: "index_camp_tag_relations_on_tag_id"
+  end
 
   create_table "camps", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -18,6 +27,12 @@ ActiveRecord::Schema.define(version: 2021_03_11_071006) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_camps_on_user_id"
+  end
+
+  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "style", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -33,5 +48,7 @@ ActiveRecord::Schema.define(version: 2021_03_11_071006) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "camp_tag_relations", "camps"
+  add_foreign_key "camp_tag_relations", "tags"
   add_foreign_key "camps", "users"
 end
