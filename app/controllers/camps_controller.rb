f2670775a4fc8661c7d0bf602e5d67f0baba2601
motoki6318 <mksk6318@icloud.com>
2021-03-11 16:25:10 +0900

    campsテーブルを作成

--- app/controllers/camps_controller.rb
+++ app/controllers/camps_controller.rb
@@ -1,10 +1,19 @@
 class CampsController < ApplicationController
   def index
+    @camps = Camp.all.includes(:user)
   end
 
   def new
+    @camp = Camp.new
   end
 
   def create
+    @camp = Camp.new
+    if @camp.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_095039) do
+ActiveRecord::Schema.define(version: 2021_03_11_071006) do
+
+  create_table "camps", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_camps_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 2021_03_10_095039) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "camps", "users"
 end
