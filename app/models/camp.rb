efcd6a3be0a3ac3a18b284d93af6cf6c9d2b8ac8
motoki6318 <mksk6318@icloud.com>
2021-03-17 18:58:14 +0900

    itemとcampの関連付け

--- app/models/camp.rb
+++ app/models/camp.rb
@@ -2,6 +2,7 @@ class Camp < ApplicationRecord
   belongs_to :user
   has_many :camp_tag_relations
   has_many :tags, through: :camp_tag_relations
+  has_many :camp_item_relations
   has_many :items, through: :camp_item_relations
   
   validates :title, presence: true
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,9 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
   belongs_to :user
+  has_many :camp_item_relations
   has_many :camps, through: :camp_item_relations
+  
 
   with_options presence: true do
     validates :genre_id, numericality: { other_than: 1, message: 'Select'}
