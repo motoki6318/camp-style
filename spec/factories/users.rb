1435dbd88ab063fb1b50f57015882e122332e300
motoki6318 <mksk6318@icloud.com>
2021-03-19 09:17:48 +0900

    gemの導入

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    nickname {"キャンプマン"}
+    email {Faker::Internet.free_email}
+    password {"test123"}
+    password_confirmation {password}
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe '#create' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+    it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できること' do
+      binding.pry
+      expect(@user).to be_valid
+    end
+
+    it 'nicknameが空では登録できないこと' do
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    end
+
+    it 'emailが空では登録できないこと' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages)
+    end
+
+    it 'passwordが空では登録できないこと' do
+
+    end
+  end
+end
