6b73d8635c19b08a6c18e04b598214b5936e4102
motoki6318 <mksk6318@icloud.com>
2021-03-30 19:05:57 +0900

    commentsコントローラーの作成

--- app/controllers/camps_controller.rb
+++ app/controllers/camps_controller.rb
@@ -25,6 +25,8 @@ class CampsController < ApplicationController
   end
 
   def show
+    @comment = Comment.new
+    
   end
 
   def edit
---
+++ spec/requests/commets_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Commets", type: :request do
+
+end
