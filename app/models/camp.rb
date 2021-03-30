09b5bba540d6a12d918be8fb4a25e2c805bf0548
motoki6318 <mksk6318@icloud.com>
2021-03-30 18:06:11 +0900

    commentモデル作成アソシエーション設定

--- app/models/camp.rb
+++ app/models/camp.rb
@@ -4,5 +4,6 @@ class Camp < ApplicationRecord
   has_many :tags, through: :camp_tag_relations
   has_many :camp_item_relations
   has_many :items, through: :camp_item_relations
+  has_many :comments
   has_one_attached :image
 end
